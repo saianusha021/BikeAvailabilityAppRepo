99f7dfbcc90234d57fa034d998cb6c55a12b96b4
Sai Anusha <saianusha021@gmail.com>
2019-06-24 16:56:52 -0400

    Committing the Bike Availability Project Created one Xcode

---
+++ BikeAvailabilityApp/BikeAvailabilityApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BikeAvailabilityApp
+//
+//  Created by Anusha Meda on 6/24/19.
+//  Copyright © 2019 Anusha Meda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
